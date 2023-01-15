74b40895dcba5e061b2d1405cb2c65f09abeaf0e
kboul <kos.voulgaridis@gmail.com>
2023-01-15 23:10:08 +0200

    Rerender Card comp only when its props change, remove anchor tag

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,21 +1,21 @@
+import { memo } from "react";
+
 import Image from "./Image";
 
 interface CardProps {
   url: string;
 }
 
-export default function Card({ url }: CardProps) {
+export default memo(function Card({ url }: CardProps) {
   return (
     <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
       <article className="overflow-hidden rounded-lg shadow-lg">
-        <a href="#">
-          <Image
-            className="block w-full h-48 object-cover"
-            placeholderImg="/loadingImage.png"
-            src={url}
-          />
-        </a>
+        <Image
+          className="block w-full h-48 object-cover"
+          placeholderImg="/loadingImage.png"
+          src={url}
+        />
       </article>
     </div>
   );
-}
+});
