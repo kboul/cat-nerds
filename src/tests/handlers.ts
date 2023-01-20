7cc9568178b9b0043ac25dd7f5221c35e34432df
kboul <kos.voulgaridis@gmail.com>
2023-01-20 19:58:28 +0200

    Unit test clicking load more button to load more at images

--- src/tests/handlers.ts
+++ src/tests/handlers.ts
@@ -1,12 +1,19 @@
 import { rest } from "msw";
 
 import { catImagesUrl, catImageUrl, favouriteUrl } from "../api";
-import { catImages, catImage, favouriteCatImages } from "./mockData";
+import {
+  catImages,
+  catImage,
+  favouriteCatImages,
+  loadMoreCatImages
+} from "./mockData";
 
 export const handlers = [
-  rest.get(`${catImagesUrl}?limit=10`, (_, res, ctx) =>
-    res(ctx.json(catImages))
-  ),
+  rest.get(`${catImagesUrl}`, (req, res, ctx) => {
+    const limit = req.url.searchParams.get("limit");
+    if (limit === "20") return res(ctx.json(loadMoreCatImages));
+    return res(ctx.json(catImages));
+  }),
   rest.get(`${catImageUrl}/6sn`, (_, res, ctx) => res(ctx.json(catImage))),
   rest.get(`${favouriteUrl}?sub_id=kboul`, (_, res, ctx) =>
     res(ctx.json(favouriteCatImages))
--- src/tests/mockData.ts
+++ src/tests/mockData.ts
@@ -1,6 +1,4 @@
-import { CatImage } from "../models";
-
-const catImages: CatImage[] = [
+const catImages = [
   {
     breeds: [],
     id: "1tv",
@@ -80,6 +78,199 @@ const catImage = {
   height: 600
 };
 
+const loadMoreCatImages = [
+  {
+    breeds: [],
+    id: "4lf",
+    url: "https://cdn2.thecatapi.com/images/4lf.gif",
+    width: 200,
+    height: 150
+  },
+  {
+    breeds: [],
+    id: "4o9",
+    url: "https://cdn2.thecatapi.com/images/4o9.jpg",
+    width: 500,
+    height: 333
+  },
+  {
+    breeds: [],
+    id: "7ct",
+    url: "https://cdn2.thecatapi.com/images/7ct.jpg",
+    width: 500,
+    height: 338
+  },
+  {
+    breeds: [],
+    id: "abt",
+    url: "https://cdn2.thecatapi.com/images/abt.jpg",
+    width: 900,
+    height: 675
+  },
+  {
+    breeds: [],
+    id: "aqc",
+    url: "https://cdn2.thecatapi.com/images/aqc.jpg",
+    width: 2000,
+    height: 1339
+  },
+  {
+    breeds: [],
+    id: "c01",
+    url: "https://cdn2.thecatapi.com/images/c01.jpg",
+    width: 720,
+    height: 900
+  },
+  {
+    breeds: [],
+    id: "e6b",
+    url: "https://cdn2.thecatapi.com/images/e6b.jpg",
+    width: 500,
+    height: 215
+  },
+  {
+    breeds: [
+      {
+        weight: { imperial: "5 - 9", metric: "2 - 4" },
+        id: "munc",
+        name: "Munchkin",
+        vetstreet_url: "http://www.vetstreet.com/cats/munchkin",
+        temperament: "Agile, Easy Going, Intelligent, Playful",
+        origin: "United States",
+        country_codes: "US",
+        country_code: "US",
+        description:
+          "The Munchkin is an outgoing cat who enjoys being handled. She has lots of energy and is faster and more agile than she looks. The shortness of their legs does not seem to interfere with their running and leaping abilities.",
+        life_span: "10 - 15",
+        indoor: 0,
+        lap: 1,
+        alt_names: "",
+        adaptability: 5,
+        affection_level: 5,
+        child_friendly: 4,
+        dog_friendly: 5,
+        energy_level: 4,
+        grooming: 2,
+        health_issues: 3,
+        intelligence: 5,
+        shedding_level: 3,
+        social_needs: 5,
+        stranger_friendly: 5,
+        vocalisation: 3,
+        experimental: 0,
+        hairless: 0,
+        natural: 0,
+        rare: 0,
+        rex: 0,
+        suppressed_tail: 0,
+        short_legs: 1,
+        wikipedia_url: "https://en.wikipedia.org/wiki/Munchkin_(cat)",
+        hypoallergenic: 0,
+        reference_image_id: "j5cVSqLer"
+      }
+    ],
+    id: "uk0SrrBbQ",
+    url: "https://cdn2.thecatapi.com/images/uk0SrrBbQ.jpg",
+    width: 2329,
+    height: 2097
+  },
+  {
+    breeds: [
+      {
+        weight: { imperial: "5 - 9", metric: "2 - 4" },
+        id: "munc",
+        name: "Munchkin",
+        vetstreet_url: "http://www.vetstreet.com/cats/munchkin",
+        temperament: "Agile, Easy Going, Intelligent, Playful",
+        origin: "United States",
+        country_codes: "US",
+        country_code: "US",
+        description:
+          "The Munchkin is an outgoing cat who enjoys being handled. She has lots of energy and is faster and more agile than she looks. The shortness of their legs does not seem to interfere with their running and leaping abilities.",
+        life_span: "10 - 15",
+        indoor: 0,
+        lap: 1,
+        alt_names: "",
+        adaptability: 5,
+        affection_level: 5,
+        child_friendly: 4,
+        dog_friendly: 5,
+        energy_level: 4,
+        grooming: 2,
+        health_issues: 3,
+        intelligence: 5,
+        shedding_level: 3,
+        social_needs: 5,
+        stranger_friendly: 5,
+        vocalisation: 3,
+        experimental: 0,
+        hairless: 0,
+        natural: 0,
+        rare: 0,
+        rex: 0,
+        suppressed_tail: 0,
+        short_legs: 1,
+        wikipedia_url: "https://en.wikipedia.org/wiki/Munchkin_(cat)",
+        hypoallergenic: 0,
+        reference_image_id: "j5cVSqLer"
+      }
+    ],
+    id: "LxcLIxuVT",
+    url: "https://cdn2.thecatapi.com/images/LxcLIxuVT.jpg",
+    width: 1671,
+    height: 912
+  },
+  {
+    breeds: [
+      {
+        weight: { imperial: "9 - 14", metric: "4 - 6" },
+        id: "pers",
+        name: "Persian",
+        cfa_url: "http://cfa.org/Breeds/BreedsKthruR/Persian.aspx",
+        vetstreet_url: "http://www.vetstreet.com/cats/persian",
+        vcahospitals_url:
+          "https://vcahospitals.com/know-your-pet/cat-breeds/persian",
+        temperament: "Affectionate, loyal, Sedate, Quiet",
+        origin: "Iran (Persia)",
+        country_codes: "IR",
+        country_code: "IR",
+        description:
+          "Persians are sweet, gentle cats that can be playful or quiet and laid-back. Great with families and children, they absolutely love to lounge around the house. While they don’t mind a full house or active kids, they’ll usually hide when they need some alone time.",
+        life_span: "14 - 15",
+        indoor: 0,
+        lap: 1,
+        alt_names: "Longhair, Persian Longhair, Shiraz, Shirazi",
+        adaptability: 5,
+        affection_level: 5,
+        child_friendly: 2,
+        dog_friendly: 2,
+        energy_level: 1,
+        grooming: 5,
+        health_issues: 3,
+        intelligence: 3,
+        shedding_level: 4,
+        social_needs: 4,
+        stranger_friendly: 2,
+        vocalisation: 1,
+        experimental: 0,
+        hairless: 0,
+        natural: 1,
+        rare: 0,
+        rex: 0,
+        suppressed_tail: 0,
+        short_legs: 0,
+        wikipedia_url: "https://en.wikipedia.org/wiki/Persian_(cat)",
+        hypoallergenic: 0,
+        reference_image_id: "-Zfz5z2jK"
+      }
+    ],
+    id: "C0YfrgcOD",
+    url: "https://cdn2.thecatapi.com/images/C0YfrgcOD.jpg",
+    width: 1920,
+    height: 1080
+  }
+];
+
 const favouriteCatImages = [
   {
     id: 101295958,
@@ -91,4 +282,4 @@ const favouriteCatImages = [
   }
 ];
 
-export { catImages, catImage, favouriteCatImages };
+export { catImages, catImage, favouriteCatImages, loadMoreCatImages };
