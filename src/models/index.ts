f0bfa702f710f2ce48895021dd380a6378037909
kboul <kos.voulgaridis@gmail.com>
2023-01-22 19:00:24 +0200

    Display BreedDetails in a table

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
-export type { default as CatBreed } from "./catBreed";
+export type { CatBreed } from "./catBreed";
+export type { Weight } from "./catBreed";
 export type { default as CatImage } from "./catImage";
 export type { default as Favourite } from "./favourite";
