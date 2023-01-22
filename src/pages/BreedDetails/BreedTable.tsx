4867ce957e5d4295f33400dc1cfbea4d370f0720
kboul <kos.voulgaridis@gmail.com>
2023-01-22 19:00:24 +0200

    Truncate bug text, add tooltip, make urls clickable and navigate to the relevant page in another tab, capitalize table keys

--- src/pages/BreedDetails/BreedTable.tsx
+++ src/pages/BreedDetails/BreedTable.tsx
@@ -1,51 +1,35 @@
 import { useId } from "react";
 
-import { CatBreed } from "../../models";
-import { Weight } from "../../models/catBreed";
-
-interface TableProps {
-  data: CatBreed;
-}
-
-type TableValue = string | Weight;
-
-const capitalize = (word: string) => {
-  if (!word) return "";
-  return word.replace(
-    /(^|_)(\w)/g,
-    ($0, $1, $2) => ($1 && " ") + $2.toUpperCase()
-  );
-};
-
-const getValue = (value: TableValue) => {
-  if (typeof value === "object")
-    return `imperial: ${value?.imperial}, metric: ${value?.metric}`;
-  if (value.length > 50) return value.substring(0, 50);
-  return value;
-};
-
-const getTitle = (value: TableValue) => {
-  return typeof value === "string" && value.length > 50 ? value : "";
-};
+import { TableProps, TableValue } from "./models";
+import { capitalize, clickableText, getTitle, getValue, isUrl } from "./utils";
 
 export default function BreedTable({ data }: TableProps) {
   const id = useId();
+
+  const handleTableValueClick = (key: string, value: TableValue) => () => {
+    if (isUrl(key, value)) window.open(value as any, "_blank");
+  };
+
   return (
     <div className="relative overflow-x-auto">
       <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <tbody>
           {Object.entries(data).map(([key, value]) => {
+            const trClickableText = clickableText(key, value);
             return (
               <tr
-                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                className={`bg-white border-b dark:bg-gray-800 dark:border-gray-700 ${trClickableText}`}
                 key={`${key}-${id}`}>
                 <th
                   className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                   scope="row">
                   {capitalize(key)}
                 </th>
-                <td className="px-6 py-4" title={getTitle(value)}>
-                  {getValue(value as TableValue)}
+                <td
+                  className={`px-6 py-4 `}
+                  onClick={handleTableValueClick(key, value)}
+                  title={getTitle(value)}>
+                  {getValue(value)}
                 </td>
               </tr>
             );
---
+++ src/pages/BreedDetails/utils.ts
@@ -0,0 +1,29 @@
+import { TableValue } from "./models";
+
+const capitalize = (word: string) => {
+  if (!word) return "";
+  return word.replace(
+    /(^|_)(\w)/g,
+    ($0, $1, $2) => ($1 && " ") + $2.toUpperCase()
+  );
+};
+
+const getValue = (value: TableValue) => {
+  if (typeof value === "object")
+    return `imperial: ${value?.imperial}, metric: ${value?.metric}`;
+  if (value.length > 50) return value.substring(0, 50);
+  return value;
+};
+
+const getTitle = (value: TableValue) => {
+  return typeof value === "string" && value.length > 50 ? value : "";
+};
+
+const isUrl = (key: string, value: TableValue) => {
+  return key.includes("url") && typeof value === "string";
+};
+
+const clickableText = (key: string, value: TableValue) =>
+  isUrl(key, value) ? "hover:underline cursor-pointer" : "no-underline";
+
+export { capitalize, getValue, getTitle, clickableText, isUrl };
