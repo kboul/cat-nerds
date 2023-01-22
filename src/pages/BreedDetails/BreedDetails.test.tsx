e45b4e4ddb4cffc959b4a82547b91fd7c0a056be
kboul <kos.voulgaridis@gmail.com>
2023-01-22 17:16:03 +0200

    Unit test BreedDetails page

---
+++ src/pages/BreedDetails/BreedDetails.test.tsx
@@ -0,0 +1,57 @@
+import userEvent from "@testing-library/user-event";
+import { rest } from "msw";
+
+import { catBreedsUrl } from "../../api";
+import { renderWithProviders, screen, server, waitFor } from "../../tests";
+import BreedDetails from "./BreedDetails";
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useParams: () => ({ breedId: "aege" })
+}));
+
+describe("loading & response success", () => {
+  // eslint-disable-next-line testing-library/no-render-in-setup
+  beforeEach(() => renderWithProviders(<BreedDetails />));
+
+  test("loading message appears initially on the screen when page loads", () => {
+    expect(screen.getByText("Loading breed details...")).toBeInTheDocument();
+  });
+
+  test("a table appears on the screen with the breed info", async () => {
+    expect(await screen.findByRole("table")).toBeInTheDocument();
+  });
+
+  test("clicking on a url navigates to the relevant page", async () => {
+    window.open = jest.fn();
+
+    const wikipediaUrl = "https://en.wikipedia.org/wiki/Aegean_cat";
+
+    const wikipediaUrlEl = await screen.findByText(wikipediaUrl);
+    await userEvent.click(wikipediaUrlEl);
+    expect(window.open).toHaveBeenCalledTimes(1);
+    expect(window.open).toHaveBeenCalledWith(wikipediaUrl, "_blank");
+  });
+});
+
+describe("response failure", () => {
+  test.skip("server error generates the appropriate message on the screen", async () => {
+    server.resetHandlers(
+      rest.get(catBreedsUrl, (req, res, ctx) => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const breedId = req.url.searchParams.get("breedId");
+        return res(ctx.status(500));
+      })
+    );
+    renderWithProviders(<BreedDetails />);
+
+    await waitFor(async () => {
+      expect(
+        await screen.findByText(
+          "There was an error while fetching the breed details.",
+          { exact: false }
+        )
+      ).toBeInTheDocument();
+    });
+  });
+});
